77cf522ac49f51286459ad3415d16a0edcdd4300
Barr Kirel <nacojerk@hotmail.com>
2018-06-04 09:32:25 +0300

    idk

--- TriviaClient/infrastructure/PipeManager.cs
+++ TriviaClient/infrastructure/PipeManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
 using System.Net;
 using System.Net.Sockets;
 using TriviaClient.Utils;
